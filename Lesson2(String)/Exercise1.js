5f2cb6ba353d70331ddc4d4c1a86154f1747100f
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-02-02 09:26:24 +0530

    Feat: Learned creating strings and worked with template strings

---
+++ Lesson2(String)/Exercise1.js
@@ -0,0 +1,18 @@
+//3c
+console.log('My name is '+'Dhamo!');
+
+//3d
+console.log('Total cost: $' + (5 + 3));
+
+//3e
+console.log(`Total cost: $${5+3}`);
+
+//3g
+console.log('Total cost: $' + ((599 + 295)/100));
+
+//3h
+console.log(`Total cost: $${(599 + 295)/100}`);
+
+//3j
+console.log(`Total cost: $${(599 + 295)/100}
+Thank you, come again!`);
