4ca7b11fc739647b77d12cfda4c676603cd1c1fa
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-02-10 20:35:01 +0530

    feat: Added functionality of showing ToDo list events in web page using DOM

--- Lesson10(Arrays&Loops)/To-do_list/scripts/script.js
+++ Lesson10(Arrays&Loops)/To-do_list/scripts/script.js
@@ -3,6 +3,27 @@ const todoList = [];
 function addList()
 {
     const list = document.querySelector('.todo-input');
-    todoList.push(list.value);
+
+    const value = list.value;
+    todoList.push(value);
+
     console.log(todoList);
+
+    list.value = '';
+
+    const items = document.querySelector('.todo-items');
+    items.innerHTML = renderToDoList(); // displaying todo list on page
+}
+
+
+function renderToDoList()
+{
+    let todoListHTML = '';
+    for(let i = 0; i < todoList.length; ++i)
+    {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`; // HTML generation
+        todoListHTML += html;
+    }
+    return todoListHTML;
 }
