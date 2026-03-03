0f93513e02065e1f2d207d368e4ea3f44f771e1c
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-03 20:11:20 +0530

    feat: Removed default files and tested formatCurrency() method

---
+++ Projects/Amazon/tests-jasmine/spec/priceTest.js
@@ -0,0 +1,15 @@
+import { formatCurrency } from "../src/price.js";
+
+describe('Test suite: formatCurrency()', () => {
+    it('Normal test case', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('Edge case: 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    it('Edge case: Decimal Cents', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
