3bb25ae15ffe6cc1d4faac658e3577047c3ab763
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-04 10:13:55 +0530

    feat: tested addToCart() for adding a new product to cart using Jasmine testing framework

---
+++ Projects/Amazon/tests-jasmine/spec/cartTest.js
@@ -0,0 +1,26 @@
+import { cart, addToCart, loadCartFromStorage } from "../src/data/cart.js";
+
+describe('test suite: addToCart()', () => {
+    it('adds an existing product to the cart', () => {
+        
+    });
+
+    it('adds a new product to the cart', () => {
+        spyOn(localStorage, 'setItem'); // SpyOn objects to monitor them
+
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([]);
+        }); // callFake will mock the functionality of spy object without affecting the original functionality
+
+        loadCartFromStorage(); // again reloading the cart[] for updated mock
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1); // checking if the addition of item is done
+
+        // expect(<spy_obj>).toHaveBeenCalledTimes(Number) - returns how many times the Spy object (function) is called
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1); // This will work only if the expected method is mocked before this
+
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'); // productId Check
+        expect(cart[0].quantity).toEqual(1); // quantity Check
+    });
+});
---
+++ Projects/Amazon/tests-jasmine/src/data/cart.js
@@ -0,0 +1,91 @@
+import { deliveryOptions } from "../../../data/deliveryOptions.js";
+
+export let cart;
+
+loadCartFromStorage();
+
+export function loadCartFromStorage()
+{
+    cart = JSON.parse(localStorage.getItem('cartItems')) || [];
+}
+
+// Save cart to localStorage
+function saveLocal()
+{
+    localStorage.setItem('cartItems', JSON.stringify(cart));
+    }
+
+// Add Items to Cart 
+export function addToCart(productId, quantity = 1)
+{
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId)
+            matchingItem = cartItem;
+    });
+
+    if(matchingItem) matchingItem.quantity += quantity;
+    else 
+    {
+        cart.push({
+            productId,
+            quantity,
+            deliveryOptionId: '1'
+        });
+    }
+    saveLocal();
+}
+
+// Remove from cart
+export function removeFromCart(productId)
+{
+    cart = cart.filter((cartItem) => productId !== cartItem.productId);
+    saveLocal();
+}
+
+// Get total cart quantity
+export function getCartQuantity()
+{
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    })
+    return cartQuantity;
+}
+
+// Modify cart item quantity
+export function modifyCartItem(productId, quantity)
+{
+    if(quantity < 0)
+    {
+        alert('Invalid Quantity!');
+        return;
+    }
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId)
+            matchingItem = cartItem;
+    });
+    if(matchingItem) matchingItem.quantity = quantity;
+    saveLocal();
+}
+
+// Get Specific item quantity
+export function getItemQuantity(productId)
+{
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId)
+            matchingItem = cartItem; 
+    });
+    if(matchingItem) return matchingItem.quantity;
+}
+
+// Update delivery option for an item
+export function updateDeliveryOption(productId, deliveryOptionId)
+{
+    const matchingItem = cart.find(cartItem => cartItem.productId === productId);
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    saveLocal();
+}
