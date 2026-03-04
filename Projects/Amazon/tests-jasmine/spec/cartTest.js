b1f0ab2d6f3feee03769b5c5db64ecbfa3cc91e9
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-04 11:08:14 +0530

    feat: tested addToCart() for adding an existing product to the cart using Jasmine testing framework

--- Projects/Amazon/tests-jasmine/spec/cartTest.js
+++ Projects/Amazon/tests-jasmine/spec/cartTest.js
@@ -2,7 +2,25 @@ import { cart, addToCart, loadCartFromStorage } from "../src/data/cart.js";
 
 describe('test suite: addToCart()', () => {
     it('adds an existing product to the cart', () => {
-        
+        spyOn(localStorage, 'setItem');
+
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([
+                {
+                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                    quantity: 3,
+                    deliveryOptionId: '1'
+                }
+            ]);
+        });
+
+        loadCartFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 2);
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(5);
     });
 
     it('adds a new product to the cart', () => {
