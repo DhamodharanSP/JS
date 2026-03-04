eb3abc87c87ae7c89f5e4761accdb94518a66a93
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-04 20:35:40 +0530

    feat: Added hooks - beforeEach() and afterEach() to run code automatically for each tests

--- Projects/Amazon/tests-jasmine/spec/checkout/orderSummaryTest.js
+++ Projects/Amazon/tests-jasmine/spec/checkout/orderSummaryTest.js
@@ -2,16 +2,19 @@ import { renderOrderSummary } from '../../../scripts/checkout/orderSummary.js';
 import { cart, loadCartFromStorage } from "../../../data/cart.js";
 
 describe('test suite: renderOrderSummary()', () => {
-    it('displays the cart', () => {
-        const testContainer = document.querySelector('.js-test-container');
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+    let testContainer;
+
+    beforeEach(() => {
+        spyOn(localStorage, 'setItem');
+
+        testContainer = document.querySelector('.js-test-container');
         testContainer.innerHTML =  `
             <div class="js-checkout-quantity"></div>
             <div class="js-order-summary"></div>
             <div class="js-payment-summary"></div>
         `;
-
-        const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-        const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([
@@ -31,7 +34,13 @@ describe('test suite: renderOrderSummary()', () => {
         loadCartFromStorage();
 
         renderOrderSummary();
+    });
 
+    afterEach(() => {
+        testContainer.innerHTML = '';
+    });
+
+    it('displays the cart', () => {
         expect(
             document.querySelectorAll('.cart-item-container').length
         ).toEqual(2);
@@ -43,42 +52,9 @@ describe('test suite: renderOrderSummary()', () => {
         expect(
             document.querySelector(`.js-test-product-quantity-${productId2}`).innerText
         ).toContain('Quantity: 2 Update  Save Delete');
-
-        testContainer.innerHTML = '';
     });
 
     it('removes a product', () => {
-        spyOn(localStorage, 'setItem');
-
-        const testContainer = document.querySelector('.js-test-container');
-        testContainer.innerHTML =  `
-            <div class="js-checkout-quantity"></div>
-            <div class="js-order-summary"></div>
-            <div class="js-payment-summary"></div>
-        `;
-
-        const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-        const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
-
-        spyOn(localStorage, 'getItem').and.callFake(() => {
-            return JSON.stringify([
-                {
-                    productId: productId1,
-                    quantity: 3,
-                    deliveryOptionId: '1'
-                },
-                {
-                    productId: productId2,
-                    quantity: 2,
-                    deliveryOptionId: '2'
-                }
-            ]);
-        });
-
-        loadCartFromStorage();
-
-        renderOrderSummary();
-
         document.querySelector(`.js-test-delete-${productId1}`).click();
 
         expect(
@@ -96,7 +72,5 @@ describe('test suite: renderOrderSummary()', () => {
         expect(cart.length).toEqual(1);
 
         expect(cart[0].productId).toEqual(productId2);
-
-        testContainer.innerHTML = '';
     });
 });
