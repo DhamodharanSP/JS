cbf95929aee68ee72c3890343a9a2773af36c4a4
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-09 17:50:13 +0530

    feat: Added Clothing class extending Product class as an inheritance and overrided the super class method as per polymorphism

--- Projects/Amazon/data/products.js
+++ Projects/Amazon/data/products.js
@@ -33,6 +33,29 @@ class Product
   {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML()
+  {
+    return '';
+  }
+}
+
+class Clothing extends Product
+{
+  sizeChartLink;
+
+  constructor(productDetails)
+  {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML()
+  {
+    return `
+      <a href=${this.sizeChartLink} target="_blank">Size chart</a>
+    `;
+  }
 }
 
 export const products = [
@@ -742,4 +765,9 @@ export const products = [
       "footwear"
     ]
   }
-].map(productDetails => new Product(productDetails)); // converting each Product details into Products
+].map(productDetails => 
+  {
+    if(productDetails.type === 'clothing') return new Clothing(productDetails);
+    else return new Product(productDetails)
+  }
+); // converting each Product details into Products
