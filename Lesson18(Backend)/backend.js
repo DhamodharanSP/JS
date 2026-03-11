41c409e688e9c9060f82af1af5534bd1a3e379bd
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-11 11:23:53 +0530

    learn: learned receiving response from a server

--- Lesson18(Backend)/backend.js
+++ Lesson18(Backend)/backend.js
@@ -2,4 +2,8 @@ const request = new XMLHttpRequest();
 
 request.open('GET', 'https://supersimplebackend.dev');
 
+request.addEventListener('load', () => {
+    console.log(request.response);
+}); // receiving 'response' from server - 'load' means, it triggers the event after loading the response from the server
+
 request.send();
