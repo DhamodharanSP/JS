683d038936df520ee5d6bdca5ef4ea99f9df9319
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-11 12:40:03 +0530

    fix: updated the code to loadProducts from backend server and function correctly using beforeAll() with done()

--- Projects/Amazon/tests/tests-jasmine/spec/checkout/orderSummaryTest.js
+++ Projects/Amazon/tests/tests-jasmine/spec/checkout/orderSummaryTest.js
@@ -1,10 +1,17 @@
 import { renderOrderSummary } from '../../../../scripts/checkout/orderSummary.js';
 import { cart, loadCartFromStorage } from "../../../../data/cart.js";
+import { loadProducts } from '../../../../data/products.js';
 
 describe('test suite: renderOrderSummary()', () => {
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
     let testContainer;
+
+    beforeAll((done) => {
+        loadProducts(() => {
+            done(); // done() lets us control when to go to next step. It'll hold other codes from execution until its called. Note: if done() hasn't been called, then no code will be executed later infinitely
+        });
+    });
 
     beforeEach(() => {
         spyOn(localStorage, 'setItem');
