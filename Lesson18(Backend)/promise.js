2e5668e7b3e9a7b552d4edd9fb47884c85df8149
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-12 09:04:47 +0530

    info: learned about Promise.all()

--- Lesson18(Backend)/promise.js
+++ Lesson18(Backend)/promise.js
@@ -1,6 +1,26 @@
 import { loadProducts } from './loadProducts.js';
 import { loadCart } from './loadCart.js';
 
+Promise.all([
+    new Promise((resolve) => {
+        console.log('start promise');
+        loadProducts(() => {
+            console.log('finished loading products...');
+            resolve('promise1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            console.log('finished loading cart...');
+            resolve('promise2');
+        });
+    })
+]).then((values) => {
+    console.log(values);
+    console.log('Next step - Rendering page');
+}); // Promise.all is used to run multiple promises in parallel and it stops execution when any one fails... then() will work after all the promises get successfully executed
+
+/*
 new Promise((resolve) => {
     console.log('start promise');
     loadProducts(() => {
@@ -22,4 +42,4 @@ new Promise((resolve) => {
     console.log('Next step - Rendering page');
 }); // multiple promises instead of multiple callbacks to avoid nested code and flatten it for better readability and maintainability
 
-
+*/
