aab86c3c34b7417c4932e58262536dc9c3353df2
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-12 19:46:03 +0530

    feat: 'error' eventListener() on HTTP request

---
+++ Lesson18(Backend)/error.js
@@ -0,0 +1,21 @@
+let products = [];
+
+export function loadProducts()
+{
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'https://error.supersimplebackend.dev/products');
+
+    request.addEventListener('load', () => {
+        products = JSON.parse(request.response);
+        console.log(products);
+    });
+
+    request.addEventListener('error', () => {
+        console.log('Unexpected error!');
+    })
+
+    request.send();
+}
+
+loadProducts();
