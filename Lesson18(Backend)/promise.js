c62e698bed276d1459bfccd21fafc8e6b6bda60e
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-12 07:42:53 +0530

    info: passed value from one promise to then() via resolve()

--- Lesson18(Backend)/promise.js
+++ Lesson18(Backend)/promise.js
@@ -5,18 +5,20 @@ new Promise((resolve) => {
     console.log('start promise');
     loadProducts(() => {
         console.log('finished loading products...');
-        resolve();
+        resolve('promise1'); // we can pass values in resolve() to use that in then()
     });
 
-}).then(() => {
+}).then((value) => {
+    console.log(value);
     return new Promise((resolve) => {
         loadCart(() => {
             console.log('finished loading cart...');
-            resolve();
+            resolve('promise2'); // the values are only accessible between two promises - current and next
         });
     });
 
-}).then(() => {
+}).then((value) => {
+    console.log(value); // accessing the value passed from previous promise
     console.log('Next step - Rendering page');
 }); // multiple promises instead of multiple callbacks to avoid nested code and flatten it for better readability and maintainability
 
