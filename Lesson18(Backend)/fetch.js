89db68f447b57d0cbade085bc723a3c01f6ca4d7
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-12 09:50:43 +0530

    info: getting data using fetch()

--- Lesson18(Backend)/fetch.js
+++ Lesson18(Backend)/fetch.js
@@ -1,8 +1,10 @@
 function fetchProducts()
 {
     fetch('https://supersimplebackend.dev/products').then((response) => {
-        console.log(response);
-    }); // fetch uses Promise on request to get the response
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+    });
 }
 
 fetchProducts();
