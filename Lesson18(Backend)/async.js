cba3097d762d86f74c1a335c6de27fcb1907a2a5
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-12 15:06:04 +0530

    info: learned accessing resolve() returns in await

--- Lesson18(Backend)/async.js
+++ Lesson18(Backend)/async.js
@@ -1,9 +1,18 @@
 import { loadProducts } from "./loadProducts.js";
+import { loadCart } from "./loadCart.js";
 
 async function loadPage()
 {
     await loadProducts(); // this line replaces - loadProducts().then(() => { ... });
     console.log('page loaded...');
+
+    const msg = await new Promise((resolve) => {
+        console.log('await is used on promises');
+        resolve('promise return');
+    })
+
+    console.log(msg);
+
     return 'async return';  // same as resolve(value)
 }
 
