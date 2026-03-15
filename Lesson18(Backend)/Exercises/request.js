f502d9ae9b4c107e50e89d2cb7732a008909ba5a
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-15 10:24:00 +0530

    feat: done exercises on GET request using XMLHttpRequest, fetch and async/await

---
+++ Lesson18(Backend)/Exercises/request.js
@@ -0,0 +1,39 @@
+const greetURL = 'https://supersimplebackend.dev/greeting';
+
+// 18a.
+export function requestGreet()
+{
+    const request = new XMLHttpRequest();
+
+    request.open('GET', greetURL);
+
+    request.addEventListener('load', () => {
+        console.log(request.response);
+    })
+
+    request.send();
+}
+
+requestGreet();
+
+// 18b.
+export function fetchGreet()
+{
+    const promise = fetch(greetURL).then((response) => {
+        return response.text();
+    }).then((message) => {
+        console.log(message);
+    });
+}
+
+fetchGreet();
+
+// 18c.
+export async function asyncGreet()
+{
+    const response = await fetch(greetURL);
+    const message = await response.text();
+    console.log(message);
+}
+
+asyncGreet();
