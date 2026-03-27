ed3cf82eab736ed6fb4e7b31ccd10c18f49c2028
DhamodharanSP <gowthamdhamo24@gmail.com>
2026-03-27 22:43:18 +0530

    info: learned Debouncing

---
+++ Advanced/Debounce/scripts/debounce.js
@@ -0,0 +1,8 @@
+export function debounce(callback, delay) 
+{
+    let timeoutId;
+    return (...args) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => callback(...args), delay);
+    }
+}
